54b2f306906fde85d525d035160f13137b10931b
Weikang Hu <Weikang Hu>
2016-05-26 14:02:46 +0200

    Added ball physics and prefabs

---
+++ Project BB/Assets/Scripts/Ball/Dodgeball.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dodgeball : MonoBehaviour
+{
+    public enum DodgeballState { active = 1, nonActive = 2, pickable = 3, hitObject};
+    private DodgeballState m_State;
+    public DodgeballState State
+    {
+        get
+        {
+            return m_State;
+        }
+        set
+        {
+            m_State = value;
+        }
+    }
+    Rigidbody m_rigidBody;
+    public float destroyTime;
+    private string ownerName;
+    public Vector3 ballVelocity;
+    [HideInInspector]
+    public bool useGravity = true;
+    [HideInInspector]
+    public float inAirTime = 4;
+
+    public bool unstoppable = false;
+
+    // Use this for initialization
+    void Start()
+    {
+       // Destroy(gameObject, destroyTime);
+    }
+
+    void Awake()
+    {
+        m_State = DodgeballState.active;
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_rigidBody.useGravity = useGravity;
+
+        SetVelocity(ballVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inAirTime -= Time.deltaTime;
+        if (inAirTime < 0 && m_State == DodgeballState.active)
+        {
+            SetNonActive();
+        }
+
+    }
+
+    private void SetHitObject()
+    {
+        m_rigidBody.useGravity = true;
+        if (!unstoppable) m_rigidBody.velocity = Vector3.zero;
+        m_State = DodgeballState.hitObject;
+        Debug.Log("Set ball hitobject");
+    }
+
+    private void SetNonActive()
+    {
+        Debug.Log("Set ball nonactive");
+        m_rigidBody.useGravity = true;
+        m_State = DodgeballState.nonActive;
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+        if (m_State == DodgeballState.active)
+        {
+            
+            if (col.gameObject.tag == "Player" && col.gameObject.name != OwnerName)
+            {
+                SetHitObject();
+                //Player p = col.gameObject.GetComponent<Player>();
+                //p.StartKnockBack(transform.position, velocityDir);
+                Debug.Log("Hit player");
+
+            }
+            //Destroy(this.gameObject);
+        }
+    }
+
+    public void SetVelocity(Vector3 p_velocity)
+    {
+        m_rigidBody.velocity = p_velocity;
+       // velocityDir = p_velocity;
+
+    }
+    public string OwnerName  { get { return this.ownerName; } set { this.ownerName = value; } }
+
+}
---
+++ Project BB/Assets/Scripts/Ball/DodgeballEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Dodgeball))]
+public class DodgeballEditor : Editor
+{
+    private bool AToggle = false;
+    private Dodgeball _evCtrl = null;
+    void OnEnable()
+    {
+        _evCtrl = (Dodgeball)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        //blablabla
+        base.OnInspectorGUI();
+        _evCtrl.useGravity = EditorGUILayout.Toggle("Use Gravity", _evCtrl.useGravity);
+        if (_evCtrl.useGravity)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(15);
+            GUILayout.Label("In Air Time", GUILayout.Width(70));
+            _evCtrl.inAirTime = EditorGUILayout.FloatField(_evCtrl.inAirTime);
+            GUILayout.EndHorizontal();
+        }
+    }
+}
